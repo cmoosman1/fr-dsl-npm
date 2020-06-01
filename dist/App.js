80835bd8f51ab6bcf7cb96f40ebed1cf7da4c501
Criss Moosman <crissmoosman@TW10226.local>
2020-06-01 10:48:33 -0700

    feat: Add github actions

--- dist/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Button } from './components';
-import './App.css';
-function App() {
-    return (React.createElement("div", { className: "App" },
-        React.createElement(Button, { primary: true }, "Test")));
-}
-export default App;
--- dist/stories/Button.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { action } from '@storybook/addon-actions';
-import { Button } from '../components/Button';
-export default {
-    title: 'Button',
-    component: Button
-};
-export var Default = function () { return React.createElement(Button, { onClick: action('clicked') }, "Default Button"); };
-export var Primary = function () { return React.createElement(Button, { primary: true, onClick: action('clicked') }, "Primary Button"); };
